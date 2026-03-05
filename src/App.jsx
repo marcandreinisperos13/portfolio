86bd896144ce11b16ee5cd70292c564ffa09f677
marc <marcandreinisperos13@gmail.com>
2026-03-06 04:19:20 +0800

    setup the necessary packages, assets, color palette

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 export default function App() {
   return (
-    <div className="h-screen flex items-center justify-center bg-black">
-      <h1 className="text-5xl font-bold text-green-500">Tailwind Working</h1>
+    <div>
+      App
     </div>
   );
 }
